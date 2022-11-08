4947477f5a7d2311e40fc9da6a8bac850ae7bd5d
AvaniSrishty <92068623+AvaniSrishty@users.noreply.github.com>
2022-11-08 13:40:46 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,33 @@
+status = "";
+room_img = "";
+function preload(){
+    img = loadImage("room_project.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(600, 450);
+    canvas.position(330, 200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 600, 500);
+}
+
+function modelLoaded(){
+    console.log("model is ready");
+    Status = true;
+    object_Detector.detect(room_img,gotResults);
+}
+
+
+function gotResults(error, result){
+    if(error){
+       console.log(error);
+    }
+
+    else{
+        console.log(result);
+    }
+}
